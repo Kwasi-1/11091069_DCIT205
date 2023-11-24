fb6c5e989e8f7f46c17f77e2bedddd8dd0736ec0
Afful Nana Kwasi <affulnana04@gmail.com>
2023-11-24 06:34:06 +0000

    made the navebar responive

---
+++ myscript.js
@@ -0,0 +1,9 @@
+// javascript to joogle navbar
+
+      var navs = document.getElementById("navs");
+      function showMenu(){
+        navs.style.right = "0";
+      }
+      function hideMenu(){
+        navs.style.right = "-200px";
+      }
