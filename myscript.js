e5b8278c5df2987734962e7c6dcb6c2c02807c8c
Afful Nana Kwasi <affulnana04@gmail.com>
2023-11-27 20:14:40 +0000

    maded changes to faulty image

--- myscript.js
+++ myscript.js
@@ -1,4 +1,5 @@
-// javascript to joogle navbar
+// javascript to toggle navbar
+
 
 var navs = document.getElementById("navs");
 function showMenu(){
@@ -8,11 +9,19 @@ function hideMenu(){
   navs.style.right = "-200px";
 }
 
-function showCourses(){
-  var list = documnet.getElementByClassName("hideCourses");
-  if (list.style.display == "none"){
-    list.style.display == "block";
-  }else{
-    list.style.display == "none";
-  }
-}
+// function showCourses(){
+//   var list = documnet.getElementByClassName("hideCourses");
+//   if (list.style.display == "none"){
+//     list.style.display == "block";
+//   }else{
+//     list.style.display == "none";
+//   }
+// }
+window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+// }
